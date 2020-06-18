aa5eb4393fd32b6cd587d8e6dfac40e590bbf0e9
Adegoke Temitope <adegoketemitope1909@gmail.com>
2020-06-18 08:57:22 +0100

    init

---
+++ src/components/MainPage.vue
@@ -0,0 +1,166 @@
+<template>
+  <div class="p-2">
+    <div
+      class="text-center container m-auto p-4 border-dashed border-gray-400 border-2"
+    >
+      <h1
+        class="font-semibold text-xl tracking-tight text-red-500 uppercase mb-2"
+      >
+        Youtube Mastery Reloaded Review
+      </h1>
+      <div class="flex justify-between mb-2">
+        <div class="text-gray-600">{{ new Date().toDateString() }}</div>
+        <div class="text-grey-600">Toluwanimi Daniel</div>
+      </div>
+      <hr />
+      <p class="my-2 text-gray-800">
+        Hello friend, how are you? I trust that you're staying safe. Today i'm reviewing a product called Youtube Channel Mastery reloaded #put your link here (https://paykstrt.com/18520/58450). This is a product i believe in, the pandemic has kept us indoors for months and people are spending their time on youtube, views have increased by about 75%. It's the number 1 video platform in the world and (the second most viewed site in the world, with 33.54Billion visits https://www.similarweb.com/website/youtube.com#overview). Stop watching videos alone. Now is the time  to create content, build your brand, increase your views on youtube and make sales, but You need to find a person who knows the business and knows how to help you reach your goals and that person is Adam Payne. He loves youtube and has been using it for 8 years. He's great at teaching and explaining his course, no surprise, because he was once a teacher. You really need to check out what Adam has to offer and give yourself a winning chance.
+      </p>
+      <h3 class="text-black m-5">
+        <a
+          :href="url"
+          class="bg-orange-400 py-2 rounded text-center block m-auto"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Purchase now!
+        </a>
+      </h3>
+      <img src="" alt="" />
+      <p class="my-2 text-gray-800">
+        Youtube Channel Mastery reloaded #put your link here (https://paykstrt.com/18520/58450) is a complete Youtube training course that contains everything you need to succeed on Youtube. It contains several modules with very etailed and instructional videos. You won't be needing another Youtube course again. The price for the Youtube Mastery Reloaded is $27 (earlybird). The price will go up to $47 seven days from today, June 17. 
+      </p>
+      <div class="modules">
+        <div class="flex flex-wrap -mx-2">
+          <div class="w-full md:w-1/2 p-2">
+            <div class="text-left shadow p-2">
+              <p class="font-bold text-grey-800">Module 1</p>
+              <p class="font-semibold text-grey-600">
+                Choosing A Niche For Easy Profit (Topic)
+              </p>
+              <p class="font-normal text-grey-600">
+                <ul>
+                  <li>How to uncover and validate profitable niches (What you'll learn)</li>
+		              <li>How to create a brand that you can spread to all of social media</li>
+		              <li>How to make sure you can get the .com domain for your brand too</li>
+                  <li>How to come up with an endless supply of ‘profitable’ video ideas even before you start</li>
+                </ul>
+              </p>
+            </div>
+          </div>
+          <div class="w-full md:w-1/2 p-2">
+            <div class="text-left shadow p-2">
+              <p class="font-bold text-grey-800">Module 2</p>
+              <p class="font-semibold text-grey-600">
+                Creating & Optimizing Your Channel
+              </p>
+              <p class="font-normal text-grey-600">
+                <ul>
+                  <li>What Youtube actually wants</li>
+                  <li>How to gain instant authority with your channel viewers</li>
+                  <li>The must-do’s when making your channel look pretty & professional</li>
+                  <li>The basic and advanced settings you must optimize</li>
+                </ul>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="flex flex-wrap -mx-2">
+          <div class="w-full md:w-1/2 p-2">
+            <div class="text-left shadow p-2">
+              <p class="font-bold text-grey-800">Module 3</p>
+              <p class="font-semibold text-grey-600">
+                Amazing Analytics
+              </p>
+              <p class="font-normal text-grey-600">
+                <ul>
+                  <li>Why you need to understand YouTube Analytics</li>
+                  <li>How to use the data to grow</li>
+                  <li>How to discover what your audience likes and dislikes</li>
+                </ul>
+              </p>
+            </div>
+          </div>
+          <div class="w-full md:w-1/2 p-2">
+            <div class="text-left shadow p-2">
+              <p class="font-bold text-grey-800">Module 4</p>
+              <p class="font-semibold text-grey-600">
+                Growing Your Channel
+              </p>
+              <p class="font-normal text-grey-600">
+                <ul>
+                  <li>How to get more subscribers</li>
+		<li>How to make your videos appear everywhere instantly.</li>
+		<li>Scheduling Mastery so you get all your videos ready in advance	</li>
+                </ul>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="flex flex-wrap -mx-2">
+          <div class="w-full p-2">
+            <div class="text-left shadow p-2">
+              <p class="font-bold text-grey-800">Bonus</p>
+              <p class="font-semibold text-grey-600">
+                Plus I’ve Got Some GREAT Bonus Content For You Too
+              </p>
+              <p class="font-normal text-grey-600">
+                <ul>
+                  <li>Keyword Research Mastery</li>
+		<li>Interactive YT Channel Creation Checklist</li>
+		<li>Youtube Ranking Myths Exposed</li>
+		<li>Socail Media Video Specs Infographic</li>
+                </ul>
+              </p>
+            </div>
+          </div>
+          </div>
+      </div>
+      <h3 class="text-black p-1 m-5 text-center">
+        <a
+          :href="url"
+          class="bg-orange-400 py-2 rounded text-center block m-auto"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Purchase now!
+        </a>
+      </h3>
+      <p class="my-2 text-gray-800">
+        There are two upgrades to this frontend product. One of them is the PLAYLIST MASTERY.  It covers everything from understanding how to silo playlists to optimizing them for traffic. Then there is the DISCOVERY MASTERY that teaches how to explode your growth with simple Youtube Discovery ads
+      </p>
+      <h3 class="font-semibold text-xl tracking-tight text-red-600 uppercase">
+        I have got this great bonus for you went purchase your product
+      </h3>
+      <div class="bonus">
+        <div class="flex flex-wrap">
+          <div class="w-full md:w-1/3">
+            <img src="" alt="" />
+            <p>Title</p>
+          </div>
+          <div class="w-full md:w-1/3">
+            <img src="" alt="" />
+            <p>Title</p>
+          </div>
+          <div class="w-full md:w-1/3">
+            <img src="" alt="" />
+            <p>Title</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      url: "https://payksrt.com/18520/58450"
+    };
+  },
+  name: "Home"
+};
+</script>
+
+<style scoped></style>
